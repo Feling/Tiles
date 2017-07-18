51661af314762efb1475a16b3f81aef4dae69860
igork <feling87@gmail.com>
2017-07-18 18:42:46 +0300

    adding tiles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+import 'hammerjs';
+import {MdGridListModule} from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -12,7 +16,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
